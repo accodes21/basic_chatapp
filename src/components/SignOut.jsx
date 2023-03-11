66ac213b6a174fd6ab4142016f35ba0ebea101a0
Aarya Chopkar <aaryachopkar@gmail.com>
2023-03-11 12:15:39 +0530

    SignIn and SignOut

---
+++ src/components/SignOut.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { auth } from '@/firebase'
+
+export default function SignOut() {
+    return auth.currentUser && (
+        <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
